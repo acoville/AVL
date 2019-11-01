8e4c1da45a09fab8bf0d84f107ce530c6c635f39
Adam Coville <adam.coville@gmail.com>
2019-11-01 22:55:06 +1030

    promising results from early tests. I think I'll be able to advance to AVL now.

--- b_node.hpp
+++ b_node.hpp
@@ -18,6 +18,10 @@ namespace AVL
         invariant comp_;
         std::shared_ptr<T> data_;
 
+        std::shared_ptr<b_node> leftChild_ {nullptr};
+        std::shared_ptr<b_node> rightChild_ {nullptr};
+        std::shared_ptr<b_node> parent_ {nullptr};
+
         public: 
 
         b_node(const T &obj, const invariant &comp) : 
@@ -60,11 +64,80 @@ namespace AVL
 
         //===================================================
 
+        /*-----------------------------------------
+
+            okay, after my troubles last time 
+            I want to be a lot more specific here. 
+            I want to make sure all pointers 
+            are pointing to the same addresses. 
+
+        ------------------------------------------*/
+
+        bool operator == (const b_node &other) const
+        {
+            bool left = leftChild_ == other.leftChild_;
+            bool right = rightChild_ == other.rightChild_;
+            bool parent = parent_ == other.parent_;
+            bool data = data_ == other.data_;
+
+            if(left and right and parent and data)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        //===================================================
+
         const T & Data() const
         {
             return *data_;
         }
 
+        //====================================================
 
+        /*----------------------------------------------------
+
+            Public API of the left child property
+
+            I got myself in a lot of trouble last time here. 
+            the b_tree needs to be able to touch the pointers, 
+            not just copies of the pointers, or else none of 
+            the parent / child pointers are pointing to the 
+            correct objects and I'm just in shared_ptr hell.
+
+            I don't need a Setter accepting a shared pointer,
+            I will handle that direclty in the rotation calls 
+            by simply assigning this pointer = to another one.
+
+        ----------------------------------------------------*/
+
+        bool HasLeftChild() const
+        {
+            return(leftChild_) ? true : false;
+        }
+
+        //--------------------------------------
+
+        b_node & LeftChild()
+        {
+            return *leftChild_;
+        }
+
+        //--------------------------------------
+
+        void SetLeftChild(const T &obj)
+        {
+            leftChild_ = std::make_shared<b_node<T, invariant>>(obj, comp_);
+        }
+
+        void SetLeftChild(std::shared_ptr<b_node<T, invariant>> &other)
+        {
+            leftChild_ = other;
+        }
     };
 }
--- test/b_nodeTest.cpp
+++ test/b_nodeTest.cpp
@@ -48,4 +48,63 @@ namespace AVL::test
         REQUIRE(node1 > node2);
     }
 
+    //===========================================================
+
+    TEST_CASE("Left Child bool operator", "[B node]")
+    {
+        auto node1 = b_node<int, std::less<int>>(25, comp);
+        
+        REQUIRE(!node1.HasLeftChild());
+
+        node1.SetLeftChild(15);
+
+        REQUIRE(node1.HasLeftChild());
+    }
+
+    //==============================================================
+
+    TEST_CASE("Equality Operator TRUE Test (raw pointer) ", "[B node]")
+    {
+        auto node1 = b_node<int, std::less<int>>(25, comp);
+        auto nodePtr = &node1;
+
+        REQUIRE(node1 == *nodePtr);
+    }
+
+    //==============================================================
+
+    TEST_CASE("Equality Operator TRUE Test (smart pointer)", "[B node]")
+    {
+        auto node1 = b_node<int, std::less<int>>(25, comp);
+        auto nodePtr = std::make_shared<b_node<int, std::less<int>>>(node1);
+
+        REQUIRE(node1 == *nodePtr);
+    }
+
+    //================================================================
+
+    TEST_CASE("Equality Operator FALSE test", "[B node]")
+    {
+        auto node1 = b_node<int, std::less<int>>(25, comp);
+        auto node2 = b_node<int, std::less<int>>(15, comp);
+
+        bool equal {node1 == node2};
+
+        REQUIRE(!equal);
+    }
+
+    //===================================================================
+
+    TEST_CASE("Set Child to a smart pointer Test", "[B node]")
+    {
+        auto root {b_node<int, std::less<int>>(25, comp)};
+        auto leftChild {b_node<int, std::less<int>>(15, comp)};
+
+        auto leftPtr {std::make_shared<b_node<int, std::less<int>>>(leftChild)};
+        auto rootPtr {std::make_shared<b_node<int, std::less<int>>>(root)};
+
+        leftPtr->SetLeftChild(rootPtr);
+
+        REQUIRE(leftPtr->LeftChild() == root);
+    }
 }
