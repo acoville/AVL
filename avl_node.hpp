47b1b4e85b75652a2e69de758f25d58bf23d908c
Adam Coville <adam.coville@gmail.com>
2019-11-03 23:39:16 +1030

    Forgot include guards.

--- avl_node.hpp
+++ avl_node.hpp
@@ -4,9 +4,12 @@
     
     identical to the b_node except that
     it adds the RotateLeft and RotateRight
-    functions
+    functions and Balance Factor functionality.
 
 ============================================*/
+
+#ifndef AVL_NODE_HPP
+#define AVL_NODE_HPP
 
 #include "b_node.hpp"
 
@@ -23,7 +26,7 @@ namespace AVL
         public:
 
             avl_node(const T &obj, const invariant &comp) 
-                : b_node(obj, comp)
+                : b_node <T, invariant>(obj, comp)
             {}
 
             //=====================================================
@@ -40,6 +43,22 @@ namespace AVL
 
             //======================================================
 
+            /*-------------------------------------------
+
+                RIGHT ROTATION
+
+                       BEFORE:              AFTER:
+
+                        [40]                 [35]
+                       /   \                /    \
+                     [35]   50            20      [40]
+                     /  \                /  \     /  \
+                   20   37              18  22   37  50
+                  /  \
+                18    22
+            
+            ---------------------------------------------*/
+
             void RotateRight()
             {
 
@@ -47,9 +66,46 @@ namespace AVL
 
             //======================================================
 
+            /*----------------------------------------
+
+                LEFT ROTATION
+
+                    BEFORE:             AFTER: 
+
+
+
+
+            ----------------------------------------*/
+
             void RotateLeft()
             {
 
             }
+
+            //======================================================
+
+            virtual void SetRightChild(const T &obj) override
+            {
+                *this->rightChild_ = std::make_shared<avl_node>(obj, *this->comp_);
+                *this->rightChild_->SetHeight(*this->height_ + 1);
+                *this->rightChild_->parent_ = std::make_shared<avl_node>(*this);
+
+                if(*this->leaf_)
+                    *this->leaf_ = false;
+            }
+
+            //=======================================================
+
+            virtual void SetLeftChild(const T &obj) override
+            {
+                *this->leftChild_ = std::make_shared<avl_node>(obj, *this->comp_);
+                *this->leftChild_->SetHeight(*this->height_ + 1);
+                *this->leftChild_->parent_ = std::make_shared<avl_node>(*this);
+
+                if(*this->leaf_)
+                    *this->leaf_ = false;
+            }
     };
 }
+
+#endif
